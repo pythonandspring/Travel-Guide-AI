83be02e535e55013547acc272727e56f9151dc5c
Jayandhan <jayandhanrajendran03@gmail.com>
2024-11-11 23:33:36 +0530

    feat : added base,index,admin pages and styles

--- ordering/urls.py
+++ ordering/urls.py
@@ -18,14 +18,15 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from . import views
 
 urlpatterns = [
     # path('admin/', include('teacher.urls')),
     # path('customer/', include('student.urls')),
-    path('', views.home, name='blog-home'),
+    # path('', views.home, name='blog-home'),
+
+    path("",include("customer.urls")),
 ]
 
+# Serve media files in development
 if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
