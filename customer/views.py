e2010a6ba47024d7024c8279b7803969edb3fe2a
pythonandspring <springboardmentor0107@gmail.com>
2024-11-12 17:49:07 +0530

    Revert "feat : added base,index,admin pages and styles"

--- customer/views.py
+++ customer/views.py
@@ -1,61 +1,12 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Student
 
-#to view response on browser
-from django.http import HttpResponse
 
-# To use Django's messaging system
-from django.contrib import messages  
+# def student_list(request):
+#     students = Student.objects.all()
+#     return render(request, 'home/student_list.html', {'students': students})
 
-# Create your views here.
-#REVERSE - AVOID HARDCODING THE URLS 
-from django.urls import reverse
-
-#for image!
-from django.conf import settings
-
-def test(request):
-    return HttpResponse("ok.")
-
-def guide(request):
-    context = {
-        'MEDIA_URL': settings.MEDIA_URL,
-    }
-    return render(request, 'travel/index.html',context)
-
-def admin_login(request):
-    if request.user.is_authenticated:
-        return redirect('admin_dashboard')  # Use the correct URL name
-
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-
-        if username == 'admin' and password == 'admin':
-            # Successful login logic
-            request.session['is_admin'] = True
-            return redirect(reverse('admin_dashboard'))  # Correct way to use reverse for redirection
-        else:
-            messages.error(request, 'Invalid username or password.')
-
-    return render(request, 'travel/adminPage.html')
-
-def admin_dashboard(request):
-    # Check if the user is logged in and is an admin
-    if not request.session.get('is_admin'):
-        # Redirect to login if not authenticated as an admin
-        messages.error(request, 'You need to log in as an admin first.')
-        return redirect('admin_login')  # Redirect to the login page if not logged in as admin
-
-    # Here you can render the actual admin dashboard page
-    return render(request, 'travel/adminDashboard.html')
-
-def admin_logout(request):
-    # Clear the session to log out the admin
-    if 'is_admin' in request.session:
-        del request.session['is_admin']
-        messages.success(request, 'You have logged out successfully.')
-    else:
-        messages.error(request, 'You are not logged in.')
-    
-    # Redirect to the login page after logging out
-    return redirect('admin_login')  # Adjust to your actual login page URL
+# def student_detail(request, student_id):
+#     student = get_object_or_404(Student, id=student_id)
+#     return render(request, 'home/student_detail.html', {'student': student})
