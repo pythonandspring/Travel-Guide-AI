13ca791d0d021908c1d77d1a66e804f511dd8dcd
Gaurav Desai <desaigaurav87@gmail.com>
2024-11-21 08:33:16 +0530

    db pass

--- travelling/settings.py
+++ travelling/settings.py
@@ -93,7 +93,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'user',
         'USER': 'root',                
-        'PASSWORD': 'gd456nds',            
+        'PASSWORD': 'root',            
         'HOST': '127.0.0.1',                    
         'PORT': '3306',   
     }
