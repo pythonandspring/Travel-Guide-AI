3e002bba3753f229f94ed6a27b8250600f1bf783
kusuma <kusumasurisetty2003@gmail.com>
2024-12-20 13:16:00 +0530

    add files

--- accommodation/api/views.py
+++ accommodation/api/views.py
@@ -1,4 +1,5 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 from accommodation.models import Hotel, HotelRoom, HotelImage
 from .serializers import HotelSerializer, HotelRoomSerializer, HotelImageSerializer
@@ -6,15 +7,18 @@ from .serializers import HotelSerializer, HotelRoomSerializer, HotelImageSeriali
 class HotelViewSet(ModelViewSet):
     queryset = Hotel.objects.all()
     serializer_class = HotelSerializer
+    authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
 
 class HotelRoomViewSet(ModelViewSet):
     queryset = HotelRoom.objects.all()
     serializer_class = HotelRoomSerializer
+    authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
     
 
 class HotelImageViewSet(ModelViewSet):
     queryset = HotelImage.objects.all()
     serializer_class = HotelImageSerializer
+    authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
