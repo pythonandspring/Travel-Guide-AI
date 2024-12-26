39063389ec8ac366710af91b6fe198a0d7d17ff6
Gaurav Desai <desaigaurav87@gmail.com>
2024-12-26 17:39:09 +0530

    latest change

--- travelling/settings.py
+++ travelling/settings.py
@@ -102,19 +102,19 @@ WSGI_APPLICATION = 'travelling.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.mysql',
-    #     'NAME': 'user',
-    #     'USER': 'root',                
-    #     'PASSWORD': 'gd456nds',                       
-    #     'HOST': '127.0.0.1',                    
-    #     'PORT': '3306',
-        
-    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': f"{BASE_DIR}/db.sqlite3",
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'user',
+        'USER': 'root',                
+        'PASSWORD': 'gd456nds',                       
+        'HOST': '127.0.0.1',                    
+        'PORT': '3306',
+        
     }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': f"{BASE_DIR}/db.sqlite3",
+    # }
 }
 
 # Password validation
