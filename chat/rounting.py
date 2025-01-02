4abc93872ebafdd0de37d2f6455d0c0a0aa5684b
pythonandspring <you@example.com>
2025-01-02 23:44:31 +0530

    removed all the code

--- chat/rounting.py
+++
@@ -1,7 +0,0 @@
-# from django.urls import re_path
-# from . import consumers
-
-# websocket_urlpatterns = [
-#     re_path(r'ws/guide/(?P<guide_id>\d+)/$',
-#             consumers.GuideConsumer.as_asgi()),
-# ]
